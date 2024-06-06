082244f82ad52f0e9f624a1645b1fa8bb380ea0a
Benjamin Janssens <benji.janssens@gmail.com>
2024-06-06 14:34:59 +0200

    fix: update getCredentials in CatalogImportClient; update API reports
    Signed-off-by: Benjamin Janssens <benji.janssens@gmail.com>

--- plugins/catalog-import/src/api/CatalogImportClient.ts
+++ plugins/catalog-import/src/api/CatalogImportClient.ts
@@ -256,7 +256,7 @@ the component will become available.\n\nFor more information, read an \
     } = options;
 
     const { token } = await this.scmAuthApi.getCredentials({
-      url: repositoryUrl,
+      host: new URL(repositoryUrl).host,
       additionalScope: {
         repoWrite: true,
       },
